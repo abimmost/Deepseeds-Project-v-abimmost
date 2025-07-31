68bd78c736a5266b754e380dd88891a4a052c9c7
abimmost <atsimbomgwe31@outlook.com>
2025-07-31 12:03:27 +0100

    Added error handling

--- 2.0 software 1.0/python intermediate/Day4/error.py
+++ 2.0 software 1.0/python intermediate/Day4/error.py
@@ -1,12 +1,41 @@
 def style():
     print("_" * 40)
 
-def number_error():
-    style()
-    try:
-        user_input = int(input("Please enter your phone number: "))
-        print(f"User input: {user_input}")
-    except ValueError:
-        print(f"Incorrect number")
+# def number_error():
+#     style()
+#     try:
+#         user_input = int(input("Please enter your phone number: "))
+#         print(f"User input: {user_input}")
+#     except ValueError:
+#         print(f"Incorrect number")
 
-number_error()
+# number_error()
+
+# def division_error():
+#     try:
+#         style()
+#         f_num = int(input("Enter first number: "))
+#         s_num = int(input("Enter second number: "))
+#         return f_num/s_num
+#     except ZeroDivisionError:
+#         return "Enter a number different from zero"
+
+# print(division_error())
+
+
+# def dict_error(data):
+#     # pass
+#     try:
+#         color = data['color']
+#         print(f"My color is {color}")
+#     except KeyError:
+#         print("Color isn't a key")
+
+# data = {
+#     "name" : "Shimasu",
+#     "age" : 23,
+#     "favmeal" : "rice" 
+# }
+
+# dict_error(data)
+# # print(dict_error())
