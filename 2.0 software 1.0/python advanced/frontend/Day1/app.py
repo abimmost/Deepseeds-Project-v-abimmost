51729b70e1dabbb3361550eaaf5355893d6fbd71
abimmost <atsimbomgwe31@outlook.com>
2025-08-06 14:56:40 +0100

    Added test code for Forms in streamlit

--- 2.0 software 1.0/python advanced/frontend/Day1/app.py
+++ 2.0 software 1.0/python advanced/frontend/Day1/app.py
@@ -18,16 +18,32 @@ import streamlit as st
 
 # print(pressed)
 
-from profile import markdown_code
+# from profile import markdown_code
 
-st.title("WELCOME")
-st.header("WELCOME")
-st.subheader("WELCOME")
-st.image("C:/Users/atsim/Videos/Captures/Screenshot 7_6_2025 6_07_22 PM.png", caption="Welcome to the app", use_column_width=True)
+# st.title("WELCOME")
+# st.header("WELCOME")
+# st.subheader("WELCOME")
+# st.image("C:/Users/atsim/Videos/Captures/Screenshot 7_6_2025 6_07_22 PM.png", caption="Welcome to the app", use_column_width=True)
 
-uploaded_image = st.file_uploader("Upload an image", type=["png","jpeg","jpg"])
+# uploaded_image = st.file_uploader("Upload an image", type=["png","jpeg","jpg"])
 
-if uploaded_image:
-    st.image(uploaded_image, caption = "Uploaded image!", use_column_width=True)
+# if uploaded_image:
+#     st.image(uploaded_image, caption = "Uploaded image!", use_column_width=True)
 
-st.markdown(markdown_code)
+# st.markdown(markdown_code)
+
+st.header("FORMS")
+
+with st.form(key="key_data"):
+    username = st.text_input(label="Username *")
+    email = st.text_input(label="Email *")
+    prompt = st.text_area(label="Provide your instructions *")
+    submit = st.form_submit_button(label="Submit")
+    
+    if submit:
+        if not username or not email or not prompt:
+            st.error("Enter all required info")
+        else:
+            st.balloons()
+            st.success("Submitted Succesfully")
+            st.write(f"My data is:{email}")
